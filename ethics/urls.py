db26252dd09253d7dadf3c44a83065c1c3446557
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-03-20 20:40:10 +0100

    Refactored URLs

--- ethics/urls.py
+++ ethics/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/proposals/'}),
-    url(r'^proposals/', include('proposals.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/proposals/'}, name='logout'),
+    url(r'^proposals/', include('proposals.urls', namespace="proposals")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- proposals/urls.py
+++ proposals/urls.py
@@ -8,28 +8,28 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
 
     # Lists 
-    url(r'^applications/$', IndexView.as_view(), name='applications'),
-    url(r'^concepts/$', ConceptsView.as_view(), name='concepts'),
     url(r'^archive/$', ArchiveView.as_view(), name='archive'),
-    url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
+    url(r'^applications/$', IndexView.as_view(), name='my_archive'),
+    url(r'^concepts/$', ConceptsView.as_view(), name='my_concepts'),
 
     # Proposal
-    url(r'^create/$', login_required(ProposalCreate.as_view())),
-    url(r'^update/(?P<pk>\d+)/$', ProposalUpdate.as_view()),
-    url(r'^delete/(?P<pk>\d+)/$', ProposalDelete.as_view()),
+    url(r'^create/$', login_required(ProposalCreate.as_view()), name='create'),
+    url(r'^update/(?P<pk>\d+)/$', ProposalUpdate.as_view(), name='update'),
+    url(r'^delete/(?P<pk>\d+)/$', ProposalDelete.as_view(), name='delete'),
+    url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
     
     # WMO
-    url(r'^wmo/create/(?P<pk>\d+)/$', WmoCreate.as_view()),
-    #url(r'^wmo/update/(?P<pk>\d+)/$', WmoUpdate.as_view()),
+    url(r'^wmo/create/(?P<pk>\d+)/$', WmoCreate.as_view(), name='wmo_create'),
+    #url(r'^wmo/update/(?P<pk>\d+)/$', WmoUpdate.as_view(), name='wmo_update'),
 
     # Study
-    url(r'^study/create/(?P<pk>\d+)/$', StudyCreate.as_view()),
-    #url(r'^study/update/(?P<pk>\d+)/$', StudyUpdate.as_view()),
+    url(r'^study/create/(?P<pk>\d+)/$', StudyCreate.as_view(), name='study_create'),
+    #url(r'^study/update/(?P<pk>\d+)/$', StudyUpdate.as_view(), name='study_update'),
 
     # Task(s)
-    url(r'^task/create/(?P<pk>\d+)/$', TaskCreate.as_view()),
-    url(r'^task/update/(?P<pk>\d+)/$', TaskUpdate.as_view()),
-    url(r'^task/delete/(?P<pk>\d+)/$', TaskDelete.as_view()),
+    url(r'^task/create/(?P<pk>\d+)/$', TaskCreate.as_view(), name='task_create'),
+    url(r'^task/update/(?P<pk>\d+)/$', TaskUpdate.as_view(), name='task_update'),
+    url(r'^task/delete/(?P<pk>\d+)/$', TaskDelete.as_view(), name='task_delete'),
 
     url(r'^members/$', MembersView.as_view(), name='members'),
     url(r'^meetings/$', MeetingsView.as_view(), name='meetings'),
