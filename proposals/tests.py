690834284bd1251002a26f75cb50e451f96c2445
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-06-02 13:58:04 +0200

    Added test for generating reference numbers

--- proposals/tests.py
+++ proposals/tests.py
@@ -18,23 +18,21 @@ class ProposalTestCase(TestCase):
     def test_reference_number(self):
         current_year = str(datetime.now().year)
 
+        # Generate a new reference number
         ref_number2 = generate_ref_number(self.user)
         self.assertEqual(ref_number2, 'test0101-02-' + current_year)
 
+        # Add a proposal fot the same, check new reference number
         p2 = Proposal.objects.create(title='p2', reference_number=ref_number2, created_by=self.user, relation=self.relation)
         ref_number3 = generate_ref_number(self.user)
         self.assertEqual(ref_number3, 'test0101-03-' + current_year)
 
+        # Delete a proposal, check new reference number
         p2.delete()
-
         ref_number4 = generate_ref_number(self.user)
         self.assertEqual(ref_number4, 'test0101-02-' + current_year)
 
-        self.p1.delete()
-
-        ref_number5 = generate_ref_number(self.user)
-        self.assertEqual(ref_number5, 'test0101-01-' + current_year)
-
+        # Add a proposal for another user
         user2 = User.objects.create_user(username='test0102', email='test@test.com', password='secret')
         p3 = Proposal.objects.create(title='p3', reference_number=generate_ref_number(user2), created_by=user2, relation=self.relation)
         self.assertEqual(p3.reference_number, 'test0102-01-' + current_year)
