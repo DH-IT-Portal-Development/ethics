82d615418e2c6e4651bd942bbcde98f8c7a06b15
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-10-06 14:08:05 +0200

    Moved mixins to mixins.py

---
+++ proposals/mixins.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.views.generic.detail import SingleObjectMixin
+
+from .models import Proposal, Task
+
+
+class LoginRequiredMixin(object):
+    """Mixin for generic views to retun to login view if not logged in"""
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+
+class UserAllowedMixin(SingleObjectMixin):
+    def get_object(self, queryset=None):
+        """
+        Checks whether the current User is in the applicants of a Proposal
+        and whether the Proposal has not yet been submitted.
+        """
+        obj = super(UserAllowedMixin, self).get_object(queryset)
+
+        applicants = []
+        status = None
+        if isinstance(obj, Proposal):
+            applicants = obj.applicants.all()
+            status = obj.status
+        elif isinstance(obj, Task):
+            applicants = obj.session.proposal.applicants.all()
+            status = obj.session.proposal.status
+        else:
+            applicants = obj.proposal.applicants.all()
+            status = obj.proposal.status
+
+        if self.request.user not in applicants or status >= Proposal.SUBMITTED:
+            raise PermissionDenied
+
+        return obj
