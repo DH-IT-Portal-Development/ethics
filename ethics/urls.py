527f4833b7737af01172c20de4557f03029b0f8c
mhkuu <M.H.vanderKlis@uu.nl>
2016-02-02 13:55:56 +0100

    Updated settings files to Django 1.8.9

--- ethics/urls.py
+++ ethics/urls.py
@@ -1,3 +1,9 @@
+"""
+URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+"""
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
--- ethics/wsgi.py
+++ ethics/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for ethics project.
+WSGI config for etcl project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ethics.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etcl.settings")
+
 application = get_wsgi_application()
