27121068600c9d4f5dc5706f9fbb28a755bb3bc4
mhkuu <M.H.vanderKlis@uu.nl>
2016-02-02 13:27:38 +0100

    Added e-mail to reviewers

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -12,7 +14,7 @@ class DecisionListView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'decisions'
 
     def get_queryset(self):
-        """Return all decisions of the current user"""
+        """Returns all Decisions of the current User"""
         return Decision.objects.filter(reviewer=self.request.user)
 
 
@@ -20,7 +22,7 @@ class SupervisorView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'decisions'
 
     def get_queryset(self):
-        """Return all the current open decisions for the current user"""
+        """Returns all the current open Decisions for the current User"""
         return Decision.objects.filter(review__date_end=None, review__stage=Review.SUPERVISOR, reviewer=self.request.user)
 
 
@@ -28,7 +30,7 @@ class CommissionView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'decisions'
 
     def get_queryset(self):
-        """Return all the current open decisions for the current user"""
+        """Returns all the current open Decisions for the current User"""
         return Decision.objects.all()  # filter(review__date_end=None, review__stage=Review.COMMISSION, reviewer=self.request.user)
 
 
@@ -39,6 +41,7 @@ class ReviewDetailView(LoginRequiredMixin, UserAllowedMixin, generic.DetailView)
     model = Review
 
     def get_context_data(self, **kwargs):
+        """Adds the results of the machine-wise review to the context."""
         context = super(ReviewDetailView, self).get_context_data(**kwargs)
         go, reasons = auto_review(self.get_object().proposal)
         context['auto_review_go'] = go
@@ -48,7 +51,7 @@ class ReviewDetailView(LoginRequiredMixin, UserAllowedMixin, generic.DetailView)
 
 class ReviewAssignView(LoginRequiredMixin, UserAllowedMixin, generic.UpdateView):
     """
-    Allows a superuser to assign reviewers.
+    Allows a User of the SECRETARY group to assign reviewers.
     """
     model = Review
     form_class = ReviewForm
@@ -57,15 +60,25 @@ class ReviewAssignView(LoginRequiredMixin, UserAllowedMixin, generic.UpdateView)
         return reverse('reviews:home')
 
     def form_valid(self, form):
-        """Update the review status, create Decisions and (TODO) send e-mail to reviewers."""
+        """Updates the review status, creates Decisions and sends e-mail to reviewers."""
         form.instance.stage = Review.COMMISSION
+        emails = []
         for user in form.cleaned_data['reviewers']:
             decision = Decision(review=form.instance, reviewer=user)
             decision.save()
+            emails.append(user.email)
+
+        subject = 'ETCL: aanstellen commissieleden'
+        message = 'Zie hier'
+        send_mail(subject, message, settings.EMAIL_FROM, [emails])
+
         return super(ReviewAssignView, self).form_valid(form)
 
 
 class DecisionUpdateView(LoginRequiredMixin, UserAllowedMixin, generic.UpdateView):
+    """
+    Allows a User to make a Decision on a Review.
+    """
     model = Decision
     form_class = DecisionForm
 
