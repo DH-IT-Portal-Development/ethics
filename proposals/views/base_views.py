79e439f44b43840dcf432fe6b89e4d76ebacd5f6
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-03-26 20:58:55 +0100

    Fix check for field required

--- proposals/views/base_views.py
+++ proposals/views/base_views.py
@@ -10,10 +10,6 @@ from django.views.decorators.csrf import csrf_exempt
 class HomeView(generic.TemplateView):
     template_name = 'proposals/index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        return context
-
 
 ################
 # AJAX callbacks
@@ -24,8 +20,7 @@ def check_requires(request):
     This call checks whether a certain value requires another input to be filled.
     """
     values = map(int, request.POST.getlist('value[]'))
-    # TODO: look at the correct app
-    model = apps.get_model('proposals', request.POST.get('model'))
+    model = apps.get_model(request.POST.get('app'), request.POST.get('model'))
     required_values = model.objects.filter(**{request.POST.get('field'): True}).values_list('id', flat=True)
     result = bool(set(required_values).intersection(values))
     return JsonResponse({'result': result})
