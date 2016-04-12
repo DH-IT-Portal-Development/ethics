8bfa040e5a2f7bf90a07216f80a7357d50dc380b
mhkuu <M.H.vanderKlis@uu.nl>
2016-04-12 11:52:56 +0200

    Added validation on Locations

--- observations/forms.py
+++ observations/forms.py
@@ -1,4 +1,7 @@
+# -*- encoding: utf-8 -*-
+
 from django import forms
+from django.utils.translation import ugettext as _
 
 from extra_views import InlineFormSet
 
@@ -34,9 +37,40 @@ class ObservationForm(ConditionalModelForm):
         self.check_dependency(cleaned_data, 'needs_approval', 'approval_institution')
 
 
+class LocationInlineFormSet(forms.BaseInlineFormSet):
+    """BaseInlineFormSet for Locations, handles validation"""
+    def clean(self):
+        """
+        - There should be at least one Location
+        - If a Registration which needs details has been checked, make sure the details are filled
+        """
+        count = 0
+        for form in self.forms:
+            cleaned_data = form.cleaned_data
+            if cleaned_data and not cleaned_data.get('DELETE', False):
+                count += 1
+
+                if cleaned_data.get('registrations'):
+                    for registration in cleaned_data.get('registrations'):
+                        if registration.needs_details and not cleaned_data.get('registrations_details'):
+                            error_message = _('Dit veld is verplicht')
+                            form.add_error('registrations_details', forms.ValidationError(error_message, code='required'))
+                            break
+
+        if count == 0:
+            first_form = self.forms[0]
+            error = forms.ValidationError(_(u'U dient op zijn minst één locatie aan te geven'), code='required')
+            if first_form.is_valid():
+                first_form.add_error('name', error)
+            else:
+                # TODO: find a way to show this error in the template
+                raise error
+
+
 class LocationsInline(InlineFormSet):
     """Creates an InlineFormSet for Locations"""
     model = Location
     fields = ['name', 'registrations', 'registrations_details']
     can_delete = True
     extra = 1
+    formset_class = LocationInlineFormSet
