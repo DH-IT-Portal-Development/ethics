9a0a43824f33f42949f4f19ad620eb22b8640161
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-04-23 20:42:36 +0200

    Added test for MaxWordsValidator

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.db import models
 from django.test import TestCase
 
-# Create your tests here.
+from .validators import MaxWordsValidator
+
+
+class ValidatorTest(TestCase):
+    def test_max_words_validator(self):
+        class Mock(models.Model):
+            test = models.TextField(validators=[MaxWordsValidator(5)])
+
+        try:
+            m = Mock(test='Dit is een test')
+            m.full_clean()
+        except ValidationError:
+            self.fail()
+
+        with self.assertRaises(ValidationError):
+            m = Mock(test='Dit is een test die faalt')
+            m.full_clean()
