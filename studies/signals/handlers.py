ef6b9a9f115ae6f4d64b3e68e2d9859834d79e1f
T.D. Mees <t.d.mees@uu.nl>
2018-10-23 09:26:39 +0200

    Fixed the post_save handler for Study .get() throws an exception, and filter() will do. Also, somehow I didn't realise the similar names of the functions

--- studies/signals/handlers.py
+++ studies/signals/handlers.py
@@ -1,14 +1,14 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from studies.models import Documents, Study
-from studies.utils import create_documents_for_study
+from studies.utils import create_documents_for_study as create_document_object
 
 @receiver(post_save, sender=Study)
 def create_documents_for_study(sender, instance, **kwargs):
-    documents = Documents.objects.get(study=instance)
+    documents = Documents.objects.filter(study=instance)
 
     if not documents:
-        create_documents_for_study(instance)
+        create_document_object(instance)
 
 
 @receiver(pre_delete, sender=Study)
