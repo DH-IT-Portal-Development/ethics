0bde0fe7398fb9336a200351c17a89b1bbaffcf6
Ty Mees <t.d.mees@uu.nl>
2020-03-05 15:11:09 +0100

    Added proper error pages

---
+++ core/error_views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def error_400(request, exception):
+    return render(request, 'error/400.html', status=400)
+
+
+def error_403(request, exception):
+    return render(request, 'error/403.html', status=403)
+
+
+def error_404(request, exception):
+    return render(request, 'error/404.html', status=404)
+
+
+def error_500(request):
+    return render(request, 'error/500.html', status=500)
