ddc57cad17e53441a46c7cd41fc6fa9dec034c88
Michael Villeneuve <michael@mvil.net>
2021-01-22 11:47:01 +0100

    Added OverwriteStorage to proposal_utils

--- proposals/utils/proposal_utils.py
+++ proposals/utils/proposal_utils.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from django.conf import settings
 from django.core.files.base import ContentFile
+from django.core.files.storage import FileSystemStorage
 from django.core.mail import send_mail
 from django.db.models import Q
 from django.urls import reverse
@@ -19,7 +20,8 @@ from studies.utils import study_urls
 __all__ = ['available_urls', 'generate_ref_number',
            'generate_revision_ref_number', 'generate_pdf',
            'check_local_facilities', 'notify_local_staff',
-           'filename_factory',]
+           'filename_factory', 'OverwriteStorage',
+           ]
 
 
 def available_urls(proposal):
@@ -399,6 +401,7 @@ def filename_factory(document_type):
         
         lastname = proposal.created_by.last_name
         refnum = proposal.reference_number
+        
         extension = '.' + original_fn.split('.')[-1][-7:] # At most 7 chars seems reasonable
         
         fn_parts = [ p for p in ['FETC',
@@ -411,3 +414,31 @@ def filename_factory(document_type):
         return '-'.join(fn_parts) + extension 
     
     return mkfn
+
+
+class OverwriteStorage(FileSystemStorage):
+
+    def get_available_name(self, name, **kwargs):
+        """Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+
+        Modified from http://djangosnippets.org/snippets/976/
+
+        This file storage solves overwrite on upload problem. Another
+        proposed solution was to override the save method on the model
+        like so (from https://code.djangoproject.com/ticket/11663):
+
+        def save(self, *args, **kwargs):
+            try:
+                this = MyModelName.objects.get(id=self.id)
+                if this.MyImageFieldName != self.MyImageFieldName:
+                    this.MyImageFieldName.delete()
+            except: pass
+            super(MyModelName, self).save(*args, **kwargs)
+        """
+        import os
+        
+        # If the filename already exists, remove it
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return super(OverwriteStorage, self).get_available_name(name, **kwargs)
