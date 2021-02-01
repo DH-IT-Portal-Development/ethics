48c66996487dfce3a49f19a198e98eaba957d20e
Michael Villeneuve <michael@mvil.net>
2021-02-01 12:50:23 +0100

    Preassessment fixes
    URL and view added for preassessment WMO to fix next_url and accidental submission

--- proposals/views/proposal_views.py
+++ proposals/views/proposal_views.py
@@ -601,7 +601,7 @@ class ProposalSubmitPreAssessment(ProposalSubmit):
         """
         # Note that the below method does NOT call the ProposalSubmit method, as that would generate the full PDF.
         success_url = super(ProposalSubmitPreAssessment, self).form_valid(form)
-        if 'save_back' not in self.request.POST:
+        if 'save_back' not in self.request.POST and 'js-redirect-submit' not in self.request.POST:
             proposal = self.get_object()
             generate_pdf(proposal, 'proposals/proposal_pdf_pre_assessment.html')
             start_review_pre_assessment(proposal)
