f11298c628f2563f272a774020ca8d9b9ef40d0f
Michael Villeneuve <m.l.villeneuve@uu.nl>
2021-03-25 12:45:41 +0100

    Progress on documents_list tag

--- reviews/templatetags/documents_list.py
+++ reviews/templatetags/documents_list.py
@@ -1,7 +1,29 @@
 from django import template
+from studies.models import Documents
+from collections import OrderedDict
 
 register = template.Library()
 
 @register.inclusion_tag('documents_list.html')
 def documents_list(review):
-    return {'review': review}
+    proposal = review.proposal
+    qs = Documents.objects.filter(proposal=proposal)
+    
+    # From CPython 3.6 dicts should be insertion-ordered anyway
+    headers_items = OrderedDict()
+    
+    headers_items['documents qs'] = [repr(qs)]
+    
+    for d in qs:
+        
+        files = [('IC', d.informed_consent),
+                 ('Briefing', d.briefing, not d.briefing),
+                 ('DIC', d.director_consent_declaration),
+                 ('DCI', d.director_consent_information),
+                 ('parents_information', d.parents_information, not d.parents_information),
+                 ]
+        
+        headers_items[d] = files
+    
+    return {'review': review,
+            'headers_items': headers_items}
