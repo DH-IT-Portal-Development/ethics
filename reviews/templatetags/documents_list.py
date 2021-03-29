a8571c6e6330fda057e0b17f191dd7f41bf2e5f7
Michael Villeneuve <m.l.villeneuve@uu.nl>
2021-03-29 11:57:25 +0200

    New documents list and compare list generator
    Still needs translations and testing

--- reviews/templatetags/documents_list.py
+++ reviews/templatetags/documents_list.py
@@ -3,6 +3,7 @@ from studies.models import Documents, Study
 from proposals.models import Proposal, Wmo
 from collections import OrderedDict
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -26,25 +27,63 @@ def simple_compare_link(obj, file):
     
     pk = obj.pk
     if proposal.parent:
-        parent_proposal = proposal.parent.pk
+        parent_proposal = proposal.parent
+        parent_pk = parent_proposal.pk
     else:
-        return reverse()
+        # Empty dict will result in empty template
+        return {}
     
-    # Proposals PDF's should pass no attribute
+    # Get parent documents item.
+    # Note that if the parent proposal has a different amount of
+    # Trajectories or extra documents this will fail.
+    # Same if the order of trajectories changes.
+    if obj_type == 'documents':
+        # Study documents
+        if obj.study:
+            parent_study = parent_proposal.study_set.get(
+                order=obj.study.order)
+            parent_pk = parent_study.pk
+        # "Extra" documents
+        else:
+            for n, d in enumerate(proposal.documents_set.filter(
+                study=None)):
+                    if obj == d:
+                        extra_index = n
+            try:
+                old_set = parent_proposal.documents_set.filter(
+                    study=None)
+                # Same index, different proposal
+                parent_pk = old_set[extra_index].pk
+            except (IndexError, AttributeError):
+                return {}
     
-    if obj_type == 'proposal':
-        return 
+    # Get parent wmo pk:
+    if obj_type == 'wmo':
+        parent_pk = parent_proposal.wmo.pk
+    
+    # We now have pk's for the old and new object
+    compare_kwargs = {'old': parent_pk,
+                      'new': pk,
+                      }
+    
+    # Otherwise, add the attribute
+    compare_kwargs['attribute'] = file.field.name
     
     # CompareDocumentsView expects the following args:
     # - old pk
     # - new pk
     # - type: wmo, proposal, documents, or observation
-    # - attribute (none for Proposal)
+    #   > This is hard-coded into the URL, so we handle it here
+    # - attribute (none for Proposal PDF)
     
-    pk = proposal.pk
-    attribute = file.__name__
+    if obj_type == 'proposal':
+        url= reverse('proposals:compare_proposal_docs', kwargs=compare_kwargs)
     
+    if obj_type == 'wmo':
+        url= reverse('proposals:compare_wmo_decision', kwargs=compare_kwargs)
     
+    if obj_type == 'documents':
+        url= reverse('proposals:compare_documents', kwargs=compare_kwargs)
     
     return {'compare_url': url}
     
@@ -55,8 +94,12 @@ def give_name(doc):
     for identification purposes.
     """
     
-    if hasattr(doc, 'study'):
-        return "Trajectory {}: {}".format(
+    proposal=doc.proposal
+    
+    if doc.study:
+        if proposal.study_set.count() == 1:
+            return _("Hoofdtraject")
+        return _("Traject {}: {}").format(
             doc.study.order,
             doc.study.name,
             )
@@ -66,7 +109,7 @@ def give_name(doc):
             study=None)):
             
             if doc == d:
-                return "Extra documents {}".format(n+1)
+                return _("Extra documenten {}").format(n+1)
 
 @register.inclusion_tag('documents_list.html')
 def documents_list(review):
@@ -112,20 +155,20 @@ def documents_list(review):
     for d in qs:
         entries = []
         files = [('Informed consent',
-                  d.informed_consent, d, 'documents'),
+                  d.informed_consent, d),
                  ('Briefing',
-                  d.briefing, d, 'documents'),
+                  d.briefing, d),
                  ('Consent declaratie directeur/departementshoofd',
-                  d.director_consent_declaration, d, 'documents'),
-                 ('Consent informatiebrief directeur/departementshoofd',
-                  d.director_consent_information, d, 'documents'),
+                  d.director_consent_declaration, d),
+                 ('Informatiebrief directeur/departementshoofd',
+                  d.director_consent_information, d),
                  ('Informatiebrief ouders',
-                  d.parents_information, d, 'documents'),
+                  d.parents_information, d),
                  ]
         
-        for (name, field) in files:
+        for (name, field, obj) in files:
             if field:
-                entries.append((name, field))
+                entries.append((name, field, obj))
         
         headers_items[give_name(d)] = entries
     
