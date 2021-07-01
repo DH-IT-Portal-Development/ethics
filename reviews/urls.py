fd43a63ab85cdeed54c551f01e1cae0eea887737
Michael Villeneuve <m.l.villeneuve@uu.nl>
2021-07-01 15:05:19 +0200

    Starting the new continuation

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,7 @@ from .views import \
     SupervisorDecisionOpenView, \
     DecisionOpenView, \
     ReviewDetailView, \
-    ReviewAssignView, ReviewCloseView, \
+    ReviewAssignView, ReviewCloseView, ReviewUnsubmitView, \
     DecisionUpdateView, ToConcludeProposalView, ChangeChamberView, \
     CreateDecisionRedirectView
 
@@ -29,6 +29,7 @@ urlpatterns = [
     path('change_chamber/<int:pk>/', ChangeChamberView.as_view(),
          name='change_chamber'),
     path('close/<int:pk>/', ReviewCloseView.as_view(), name='close'),
+    path('unsubmit/<int:pk>/', ReviewUnsubmitView.as_view(), name='unsubmit'),
 
     path('decide/<int:pk>/', DecisionUpdateView.as_view(), name='decide'),
     path('decide/new/<int:review>/', CreateDecisionRedirectView.as_view(),
