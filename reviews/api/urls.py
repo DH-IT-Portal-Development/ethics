4a6df7eb1db1cb3947099800bacefc72065c3cba
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2021-08-10 09:27:18 +0200

    Implemented new 'all running reviews' view

--- reviews/api/urls.py
+++ reviews/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import MyDecisionsApiView, MyOpenDecisionsApiView, \
-    OpenDecisionsApiView, OpenSupervisorDecisionApiView, \
-    ToConcludeReviewApiView, AllReviewsApiView
+from .views import AllOpenReviewsApiView, MyDecisionsApiView, \
+    MyOpenDecisionsApiView, OpenDecisionsApiView, \
+    OpenSupervisorDecisionApiView, ToConcludeReviewApiView, AllReviewsApiView
 
 app_name = 'api'
 
@@ -13,13 +13,16 @@ urlpatterns = [
     path('<str:committee>/my_open/',
          MyOpenDecisionsApiView.as_view(),
          name='my_open'),
-    path('<str:committee>/open/',
+    path('<str:committee>/open_decisions/',
          OpenDecisionsApiView.as_view(),
          name='open'),
     path('<str:committee>/open_supervisors/',
          OpenSupervisorDecisionApiView.as_view(),
          name='open_supervisors'),
 
+    path('<str:committee>/open/',
+         AllOpenReviewsApiView.as_view(),
+         name='all_open'),
     path('<str:committee>/to_conclude/',
          ToConcludeReviewApiView.as_view(),
          name='to_conclude'),
