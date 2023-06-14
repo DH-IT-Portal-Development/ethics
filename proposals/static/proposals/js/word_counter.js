501502c25280bbc094329f03b43bb804c85ad48d
Edo Storm <emtstorm@hotmail.com>
2023-06-14 10:34:10 +0200

    Implemented a working version of the seperate word counter static .js file. Works as it should now!

--- proposals/static/proposals/js/word_counter.js
+++ proposals/static/proposals/js/word_counter.js
@@ -1,15 +1,20 @@
-function word_counter(element_id, translated_string) {    
-    $(function () {    
-        // Add a running word count to the textField
-        let textField = $(element_id);
+function wordCounter(element, translated_string) {    
+    $(function () {  
+          
+        // Add a running word count to a textfield
+        // The element argument should be the name of the textField in the model
+        // The translated string should be a translateable string variable. 
+        // It needs to be declared outside of this function so that it can be translated in the template
+
+        let textField = $("#id_" + element);
         if (textField.length) {
-        textField.after("<div id='wordcount_textField' />");
+        textField.after("<div id='wordcount_" + element + "' />");
         textField[0].addEventListener("input", function () {
             var wordCount = 0;
             if (this.value.trim()) {
             wordCount = this.value.match(/\S+/g).length;
             }
-            $("#wordcount_textField").text(translated_string + wordCount);
+            $("#wordcount_" + element).text(translated_string + wordCount);
         }, false);
         }
     });
