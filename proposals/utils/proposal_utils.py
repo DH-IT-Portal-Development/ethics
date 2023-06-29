6cbd293cdd0b62779c728c9fbdd233e1b2aeb543
Edo Storm <emtstorm@hotmail.com>
2023-06-29 12:48:14 +0200

    Made the nav bar available on the translated forms page.

--- proposals/utils/proposal_utils.py
+++ proposals/utils/proposal_utils.py
@@ -99,9 +99,6 @@ def available_urls(proposal):
 
         consent_translation_url = AvailableURL(title=_('Vertaling'), url=reverse('studies:translated', args=(proposal.pk,)))
         consent_url = AvailableURL(title=_('Documenten'), children=[consent_translation_url])
-        # consent_url.children.extend(
-        #     consent_translation_url.children
-        # )
 
         data_management_url = AvailableURL(title=_('Datamanagement'))
         submit_url = AvailableURL(title=_('Versturen'))
