1a9f9cce0b5fd9e40f4fdb0bf1bbe8bba23fdcaf
Edo Storm <emtstorm@hotmail.com>
2023-09-21 15:12:40 +0200

    little improvement of little function

--- proposals/templatetags/diff_tags.py
+++ proposals/templatetags/diff_tags.py
@@ -10,15 +10,10 @@ def zip_equalize_lists(a, b):
     A zip implementation which will not stop when reaching the end of the
     smallest list, but will append None's to the smaller list to fill the gap
     """
-    try:
-        a = list(a)
-    except TypeError:
-        a = [a]
-    try:
-        b = list(b)
-    except TypeError:
-        b = [b]
 
+    a = [None] if a is None else list(a)
+    b = [None] if b is None else list(b)
+    
     a_len = len(a)
     b_len = len(b)
     diff = abs(a_len - b_len)
