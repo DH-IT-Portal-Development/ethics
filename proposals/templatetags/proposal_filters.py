9c42b830776c4b6b149f7f3e8bb375707c1a5163
Edo Storm <emtstorm@hotmail.com>
2023-10-10 10:29:41 +0200

    Incorporating changes requested by Ty

--- proposals/templatetags/proposal_filters.py
+++ proposals/templatetags/proposal_filters.py
@@ -14,6 +14,7 @@ def needs_details(selected_values, field='needs_details'):
             return True
     return False
 
+
 @register.filter
 def medical_traits(selected_values, field='medical_traits'):
     for sv in selected_values:
@@ -32,6 +33,7 @@ def has_adults(study):
 def necessity_required(study):
     age_groups = study.age_groups.values_list('id', flat=True)
     return check_necessity_required(study.proposal, age_groups, study.legally_incapable)
+
 
 @register.simple_tag
 def show_yesno(doubt=False):
