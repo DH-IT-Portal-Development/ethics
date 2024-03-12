5c40fcc2e3722bf1aacaafd996465b749fd33512
Edo Storm <emtstorm@hotmail.com>
2024-03-12 14:55:21 +0100

    Merge acception

--- reviews/views.py
+++ reviews/views.py
@@ -187,7 +187,7 @@ class CommitteeMembersWorkloadView(
                 "decision",
                 filter=base_filter & Q(decision__review__proposal__is_revision=True),
             ),
-        )
+        ).exclude(total=0)
 
 
 class SupervisorDecisionOpenView(BaseDecisionListView):
