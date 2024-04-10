134804e5b7688167aeaa66005ddeb0d038ef36a7
Edo Storm <emtstorm@hotmail.com>
2024-04-10 11:48:01 +0200

    formatting

--- studies/migrations/0028_alter_documents_informed_consent.py
+++ studies/migrations/0028_alter_documents_informed_consent.py
@@ -8,13 +8,22 @@ import proposals.utils.proposal_utils
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('studies', '0027_auto_20230227_1425'),
+        ("studies", "0027_auto_20230227_1425"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='documents',
-            name='informed_consent',
-            field=models.FileField(blank=True, help_text='Bij algemeen belang, dien hier s.v.p. een leeg Word document in.', storage=proposals.utils.proposal_utils.OverwriteStorage(), upload_to=proposals.utils.proposal_utils.FilenameFactory('Informed_Consent'), validators=[main.validators.validate_pdf_or_doc], verbose_name='Upload hier de toestemmingsverklaring (in .pdf of .doc(x)-formaat)'),
+            model_name="documents",
+            name="informed_consent",
+            field=models.FileField(
+                blank=True,
+                help_text="Bij algemeen belang, dien hier s.v.p. een leeg Word document in.",
+                storage=proposals.utils.proposal_utils.OverwriteStorage(),
+                upload_to=proposals.utils.proposal_utils.FilenameFactory(
+                    "Informed_Consent"
+                ),
+                validators=[main.validators.validate_pdf_or_doc],
+                verbose_name="Upload hier de toestemmingsverklaring (in .pdf of .doc(x)-formaat)",
+            ),
         ),
     ]
