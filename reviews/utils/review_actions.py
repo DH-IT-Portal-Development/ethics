a0b3c721daa343fbf65782beff596dd383e065d5
Edo Storm <emtstorm@hotmail.com>
2024-04-17 11:31:21 +0200

    fix: incorrect if statement on DecideAction

--- reviews/utils/review_actions.py
+++ reviews/utils/review_actions.py
@@ -85,7 +85,7 @@ class DecideAction(ReviewAction):
         user = self.user
         review = self.review
 
-        if review.stage in (review.Stages.COMMISSION, review.Stages.SUPERVISOR):
+        if review.stage not in (review.Stages.COMMISSION, review.Stages.SUPERVISOR):
             return False
 
         try:
