4cca9c147a88763a6a30ea5aecd698ec1bd5e706
Edo Storm <emtstorm@hotmail.com>
2024-10-24 15:24:00 +0200

    feat: create all InformationLetter kinds

--- attachments/kinds.py
+++ attachments/kinds.py
@@ -19,14 +19,53 @@ class StudyAttachmentKind(AttachmentKind):
     attached_object = Study
     attachment_class = StudyAttachment
 
+#####################
+# Information letters
+#####################
 
-class InformationLetter(StudyAttachmentKind):
+class InformationLetterAnonymous(StudyAttachmentKind):
 
-    db_name = "information_letter"
-    name = _("Informatiebrief")
-    description = _("Omschrijving informatiebrief")
+    db_name = "information_letter_anonymous"
+    name = _("Informatiebrief anoniem onderzoek")
+    description = _("Je verzamelt en verwerkt de gegevens van je deelnemers"
+                    " anoniem. Je moet je deelnemers dan wél informeren op"
+                    " ethische gronden (zodat ze kunnen beslissen of ze"
+                    " vrijwillig willen meedoen), maar omdat de AVG niet"
+                    " van toepassing is op anonieme gegevens, hoef je geen"
+                    " informatie te verstrekken over de verwerking van"
+                    " persoonsgegevens.")
     desiredness = desiredness.REQUIRED
 
+class InformationLetterPublicInterest(StudyAttachmentKind):
+
+    db_name = "information_letter_public_interest"
+    name = _("Informatiebrief algemeen belang")
+    description = _("Je kiest ervoor om de verwerking van persoonsgegevens te"
+                    " baseren op het algemeen belang. In beginsel is dit"
+                    " de standaardwerkwijze. \n"
+                    "Let op! Voor bepaalde aspecten van je onderzoek kan het"
+                    " desondanks nodig zijn om toestemming te vragen.")
+    desiredness = desiredness.REQUIRED
+
+class InformationLetterConsent(StudyAttachmentKind):
+
+    db_name = "information_letter_consent"
+    name = _("Informatiebrief toestemming")
+    description = _("Er zijn redenen om de verwerking van persoonsgegevens"
+                    " te baseren op toestemming van de deelnemers."
+                    " Bijvoorbeeld als er gevoelige data wordt verzameld of"
+                    " er met minderjarigen wordt gewerkt.")
+    desiredness = desiredness.REQUIRED
+
+LEGAL_BASIS_KIND_DICT = {
+    Study.LegalBases.ANONYMOUS: InformationLetterAnonymous,
+    Study.LegalBases.CONSENT: InformationLetterConsent,
+    Study.LegalBases.PUBLIC_INTEREST: InformationLetterPublicInterest,    
+}
+
+###############
+# Consent forms
+###############
 
 class ConsentForm(AttachmentKind):
 
@@ -61,7 +100,9 @@ class OtherProposalAttachment(ProposalAttachmentKind):
 
 
 STUDY_ATTACHMENTS = [
-    InformationLetter,
+    InformationLetterAnonymous,
+    InformationLetterPublicInterest,
+    InformationLetterConsent,
     ConsentForm,
 ]
 
