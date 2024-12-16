338daf7d3f89590fce5e88130b012c2eae5dc1ca
Edo Storm <emtstorm@hotmail.com>
2024-12-16 10:36:59 +0100

    fix: raise permission denied for archive access mixin

--- main/views.py
+++ main/views.py
@@ -425,6 +425,7 @@ class HumanitiesOrPrivilegeRequiredMixin(UserPassesTestMixin):
     Checks for Humanities faculty affiliation, but also lets in users belonging
     to a privileged set of groups.
     """
+    raise_exception = True
 
     def test_func(self, user):
         return can_view_archive(user)
