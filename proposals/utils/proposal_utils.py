871a7a9fff7fd21d11c0e3274104c163dc20523b
Edo Storm <emtstorm@hotmail.com>
2025-05-27 16:47:53 +0200

    style: black

--- proposals/utils/proposal_utils.py
+++ proposals/utils/proposal_utils.py
@@ -368,7 +368,7 @@ class FilenameFactory:
         refnum = proposal.reference_number
 
         # We need to make sure that we only use characters for the filename that
-        # that are in ISO 8859-1. Otherwise, a bug occurs, where the 
+        # that are in ISO 8859-1. Otherwise, a bug occurs, where the
         # OverwriteStorage filename gets served to the user.
 
         lastname = remove_non_ISO_8859_1_chars(proposal.created_by.last_name)
@@ -412,11 +412,12 @@ class OverwriteStorage(FileSystemStorage):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return super(OverwriteStorage, self).get_available_name(name, **kwargs)
-    
+
+
 def remove_non_ISO_8859_1_chars(string):
     """
     We sometimes need to make sure that a string only uses characters that
-    that are in ISO 8859-1 AKA the first 256 chars of unicode. If there are 
+    that are in ISO 8859-1 AKA the first 256 chars of unicode. If there are
     other special characters in a string, we replace them with '_'.
     """
     return "".join([char if ord(char) <= 255 else "_" for char in string])
