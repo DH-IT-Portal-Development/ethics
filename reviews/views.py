89e796af236489c741afadcc77dd7c706ee2275c
Frank Roordink <frank.roordink@live.nl>
2025-08-08 11:24:05 +0200

    comment removed after feedback

--- reviews/views.py
+++ reviews/views.py
@@ -422,7 +422,6 @@ class ReviewDiscontinueView(ReviewSidebarMixin, GroupRequiredMixin, generic.Upda
     def form_valid(self, form):
         "Sets the discontinued continuation on the review"
         review = form.instance
-        # mark_reviewed should already have been done at this point.
         discontinue_review(review)
 
         return super().form_valid(form)
