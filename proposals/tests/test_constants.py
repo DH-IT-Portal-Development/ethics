caffec7ceddd99017b2169d7a9dd549a32860d06
Frank Roordink <frank.roordink@live.nl>
2025-11-14 15:09:22 +0100

    small comment

--- proposals/tests/test_constants.py
+++ proposals/tests/test_constants.py
@@ -1,5 +1,5 @@
 # Constants that matches the fixtures
-# If a fixture's name/description gets changed it only needs to be changed here.
+# If a fixture's name/description gets changed it only needs to be changed here as far as tests go.
 
 
 # proposals/fixtures/relations.json
