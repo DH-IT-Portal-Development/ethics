107010fc4bdf712623761a96a2e333a22869e054
Frank Roordink <frank.roordink@live.nl>
2026-01-13 15:08:27 +0100

    refactor: removed asserts that are no longer needed.

--- reviews/tests.py
+++ reviews/tests.py
@@ -224,66 +224,42 @@ class AutoReviewTests(BaseReviewTestCase):
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 1)
-        self.assertEqual(
-            reasons[-1], "De aanvraag bevat het gebruik van wilsonbekwame volwassenen."
-        )
 
         self.study.deception = YesNoDoubt.DOUBT
         self.study.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 2)
-        self.assertEqual(reasons[-1], "De aanvraag bevat het gebruik van misleiding.")
 
         self.study.hierarchy = True
         self.study.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 3)
-        self.assertEqual(
-            reasons[-1],
-            "Er bestaat een hiërarchische relatie tussen de onderzoeker(s) en deelnemer(s)",
-        )
 
         self.study.has_special_details = True
         self.study.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 4)
-        self.assertEqual(
-            reasons[-1],
-            "Het onderzoek verzamelt bijzondere persoonsgegevens.",
-        )
 
         self.study.has_traits = True
         self.study.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 5)
-        self.assertEqual(
-            reasons[-1],
-            "Het onderzoek selecteert deelnemers op bijzondere kenmerken die wellicht verhoogde kwetsbaarheid met zich meebrengen.",
-        )
 
         self.study.risk = YesNoDoubt.YES
         self.study.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 6)
-        self.assertEqual(
-            reasons[-1],
-            "De onderzoeker geeft aan dat er mogelijk kwesties zijn rondom de veiligheid van de deelnemers tijdens of na het onderzoek.",
-        )
 
         self.proposal.researcher_risk = YesNoDoubt.YES
         self.proposal.save()
 
         reasons = auto_review(self.proposal)
         self.assertEqual(len(reasons), 7)
-        self.assertEqual(
-            reasons[-1],
-            "De onderzoeker geeft aan dat er mogelijk kwesties zijn rondom de veiligheid van de betrokken onderzoekers.",
-        )
 
         self.study.negativity = YesNoDoubt.YES
         self.study.save()
